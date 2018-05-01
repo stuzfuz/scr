57d0f6cf4efbc37c1120f124f8509fdbcbbf3ac3
georg <bumzacktrara@gmail.com>
2018-05-01 23:12:51 +0200

    added username to ouput data for main

--- server/components/main/MainController.php
+++ server/components/main/MainController.php
@@ -3,53 +3,15 @@
 class MainController extends SimpleController {
 
     protected function gatherData() {
-        // echo "<br> MainControiller - in 'gather data'";
-
-        // \Util::my_var_dump($this->db_conn, "MainController this->db_conn  = ");
-
-        // TODO: only those for the user who is logged in
-        $sql = "SELECT id, name  FROM channel WHERE deleted = 0 ORDER BY name";
-        $res = \DatabaseManager::query($this->db_conn, $sql, array());
-        //  \Util::my_var_dump($res, "res = ");
-        // exit();
-
-        // echo "<br> MainControiller - in 'gather data' after  query";
-        $channels = array();
-        $data = array(); 
-        if ($res->rowCount() == 0) {
-            $data["channelsfound"] = false; 
+        $ret = null; 
+        if (\AuthenticationManager::isAuthenticated()) {
+            $user = \AuthenticationManager::getAuthenticatedUser();
+            $ret["username"] = '@' . $user->getUserName();
+            $ret["loggedin"] = true;
         } else {
-            // echo "<br><br> adding channels to array ... <br>";
-            while ($channel = \DatabaseManager::fetchAssoz($res)) {
-                // \Util::my_var_dump($channel, "MainController channel  = ");
-                $channel["nameasurl"] = urlencode($channel["name"]);
-                $channels[] = $channel; 
-            }
-            
-            $data["channelsfound"] = true; 
-            $data["channels"] = $channels; 
+            $ret["loggedin"] = false;
         }
-
-        $sql = "SELECT id, txt, user_id AS userid FROM message WHERE deleted = 0 ";
-        $res = \DatabaseManager::query($this->db_conn, $sql, array());
-        //  \Util::my_var_dump($res, "res = ");
-        // exit();
-
-        // // echo "<br> MainControiller - in 'gather data' after  query";
-        $messages = array();
-        if ($res->rowCount() == 0) {
-            $data["messagesfound"] = false; 
-        }  else {
-            // echo "<br><br> adding channels to array ... <br>";
-            while ($msg = \DatabaseManager::fetchAssoz($res)) {
-                // \Util::my_var_dump($channel, "MainController channel  = ");
-                $messages[] = $msg; 
-            }
-            
-            $data["messagesfound"] = true; 
-            $data["messages"] = $messages; 
-        }
-        $this->data = $data; 
+        $this->data = $ret; 
         // \Util::my_var_dump($this->data, "MainController this->data  = ");
     }
 
