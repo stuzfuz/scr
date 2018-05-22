2a00da98cdb37675172da370e3f6b469a4c88f6e
georg <bumzacktrara@gmail.com>
2018-05-22 19:31:55 +0200

    use lower char for database name

--- DocumentRoot/server/lib/ApplicationConfig.php
+++ DocumentRoot/server/lib/ApplicationConfig.php
@@ -2,7 +2,7 @@
 
 class ApplicationConfig {
 
-    public static $databaseName = 'fh_2018_scm4_S1610307036';
+    public static $databaseName = 'fh_2018_scm4_s1610307036';
     public static $databaseUsername = 'fh_2018_scm4';
     public static $databasePassword = 'fh_2018_scm4';
     public static $databaseHost = 'mariadb';
